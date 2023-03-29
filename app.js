9c50f32857845b7b2cd118e6cfca6e4b963854cf
Ratchaporn <hrkfan321@gmail.com>
2023-03-29 23:04:00 +0700

    app.js (Attempt to add user data to mongobd)

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const app = express();
 const bodyParser = require("body-parser");
 const connect = require("mongoose");
 const db = require('./config/db.js');
+const { name } = require("ejs");
+const { MongoClient } = require("mongodb");
+
 const usersSchema = require('./model/listItem.js').users;
 const foodSchema = require('./model/listItem.js').food;
 const cartSchema = require('./model/listItem.js').cart;
@@ -40,7 +43,33 @@ app.get("/delivery" ,(req,res) =>{
 })
 
 app.get("/register" ,(req,res) =>{
-    res.render('register')
+   res.render('register')
+})
+app.post("/register" , async (req, res) =>{
+    const uri = 'mongodb://127.0.0.1:27017/TiguSDB';
+    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+   try {
+    await client.connect();
+
+    const collection = client.db('TiguSDB').collection('users');
+    const user = {
+        name: req.body.FirstName,
+        username: req.body.Username,
+        email: req.body.Email,
+        password: req.body.Password,
+        address: req.body.ddress
+    };
+
+    const result = await collection.insertOne(user);
+    console.log(`Inserted ${result.insertedCount} documents`);
+
+    res.redirect('/');
+   } catch (error) {
+    console.error(error);
+    res.status(500).send('Error submitting data');
+   } finally {
+    await client.close();
+   }
 })
 
 app.get("/admin" ,(req,res) =>{
